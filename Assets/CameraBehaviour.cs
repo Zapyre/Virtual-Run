72d2060e307ad2bf417ba8bc98ad2a01f94301b1
John <zapyre_1@hotmail.com>
2015-12-07 00:16:54 -0500

    Commiting all items

---
+++ Assets/CameraBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraBehaviour : MonoBehaviour {
+    private GameObject mainCamera;
+    private Vector3 playerPosition;
+	// Use this for initialization
+	void Start () {
+        mainCamera = GameObject.Find("Main Camera");
+        playerPosition = GameObject.Find("Player").transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        mainCamera.transform.position = new Vector3(playerPosition.x, playerPosition.y + 9.0f, playerPosition.z);
+        Debug.Log(mainCamera.transform.position);
+        Debug.Log(playerPosition);
+    }
+}
---
+++ Assets/PlayerBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBehaviour : MonoBehaviour {
+
+    public float thrust;
+    public Vector3 eulerAngleVelocity;
+    public Rigidbody rb;
+    public bool w,s,a,d;
+    // Use this for initialization
+    void Start () {
+        thrust = 5.0f;
+        rb = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(new Vector3(0, 0, thrust));
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(new Vector3(0, 0, -thrust));
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(new Vector3(-thrust, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(new Vector3(thrust, 0, 0));
+        }
+    }
+}
